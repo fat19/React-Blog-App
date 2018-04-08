3d97d077a275fa8379638246d575b5b1f8fbe945
fat19 <altin.fth19@gmail.com>
2018-04-09 00:16:27 +0300

    redux form da ilerledik

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import reducers from './reducers';
-import PostsIndex from './components/postsIndex';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new'
+import ReduxPromise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter >
       <div> 
-        <Route path="/" component={PostsIndex}/>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
